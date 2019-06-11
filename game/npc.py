795743514c7fb130dcfc4e6e9a615ae69a69c29a
Ryan Moodie <ryanmoodie@gmail.com>
2019-06-11 18:57:22 +0100

    Started altering switch case structure of cloud random(ish) walk. Use dictionary eventually?

--- game/npc.py
+++ game/npc.py
@@ -11,18 +11,21 @@ class NPC(character.Character):
         self.counter = randrange(self.max_counter)
 
     def choose_direction(self):
+        lst = [1] * 4
+        weights = [lst[:i] + [4] + lst[i:] for i in range(5)] + [[1] * 5]
+
         if 0 < self.counter < 500:
-            return [1, 1, 4, 1, 1]
+            return weights[0]
         elif 500 < self.counter < 1000:
-            return [1, 1, 1, 4, 1]
+            return weights[1]
         elif 1000 < self.counter < 1500:
-            return [1, 1, 1, 1, 4]
+            return weights[2]
         elif 1500 < self.counter < 2000:
-            return [4, 1, 1, 1, 1]
+            return weights[3]
         elif 2000 < self.counter < 2500:
-            return [1, 4, 1, 1, 1]
+            return weights[4]
         else:
-            return [1] * 5
+            return weights[5]
 
     def update_obj(self, dt):
         direction = choices(range(0, 5), weights=self.choose_direction())[0]
